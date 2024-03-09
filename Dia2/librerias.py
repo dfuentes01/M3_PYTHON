83b15fcbc2e9381e88fbf6bbedfdf62dcd0fb4ad
David Fuentes <dfuentes.proyectos@gmail.com>
2024-03-08 22:02:57 -0300

    clase dia2

---
+++ Dia2/librerias.py
@@ -0,0 +1,18 @@
+#las importaciones se agregan en las primeras lineas de codigo
+import os
+import math
+# from math import sqrt
+
+#operadores 
+print(2**4) #16 exponenciacion
+print(5//2) #2 division, retorna solo la parte entera
+print(5/2)
+print(5%2) #1,model
+
+print(os.get_blocking) #
+print(math.sqrt(9)) #calcula la raiz cuadrada y el resultado en formato "float"
+# print(ceil(3.1416)) #importacion especifica de una libreria
+print(math.ceil(3.1416)) #ceil aproxima al entero superior
+
+
+
