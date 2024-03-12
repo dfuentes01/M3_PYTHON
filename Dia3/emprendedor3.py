cc4b40350601d19b9cc0c37dce04ae8223a3c726
David Fuentes <dfuentes.proyectos@gmail.com>
2024-03-11 21:59:37 -0300

    revision de desafios

--- Dia3/emprendedor3.py
+++ Dia3/emprendedor3.py
@@ -1,10 +1,18 @@
 import math
 
-P = float(input("Ingrese el Precio de Suscripción: "))
-U = float(input("Ingrese el Numero de Usuarios: "))
+
+
+P = int(input("Ingrese el Precio de Suscripción: "))
+U = int(input("Ingrese el Numero de Usuarios: "))
 GT= float(input("Ingrese el Gasto Total: "))
-U_anterior= float(input("Ingrese las Utilidades del año Anterior: "))
+U_anterior= float(input("Ingrese las Utilidades del año Anterior:$() "))
 utilidades_actuales = (P*U-GT)
 razon_utilidades= utilidades_actuales/U_anterior
 
 print("La razon entre las utilidades actuales y las del año enterior es", razon_utilidades)
+
+# if U_anterior > 0 : 
+
+# else:
+    #realizar accion por default
+        # print("Las utilidades no pueden ser iguales a cero")
