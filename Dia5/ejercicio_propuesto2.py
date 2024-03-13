549285115034bd5a780f4cf672867926526f7fe0
David Fuentes <dfuentes.proyectos@gmail.com>
2024-03-13 19:01:38 -0300

    se realiza actividad asicronica dia5

---
+++ Dia5/ejercicio_propuesto2.py
@@ -0,0 +1,23 @@
+
+# Inicio del programa
+inicio
+
+# Identificar Variables
+base, altura, area
+
+# Recopilar información
+escribir("Ingresar base de triangulo en cm: ")
+Leer(base)
+
+escribir("Ingresar altura de triangulo cm: ")
+Leer(altura)
+
+# Calcular area del triangulo
+area=(base*altura)/2
+
+# Mostrar los resultados del calculo
+Escribir("El area del triangulo es", area, "cm2")
+
+# Fin del programa
+fin
+
---
+++ Dia5/ejercicio_propuesto4.py
@@ -0,0 +1,20 @@
+
+# Inicio del programa
+inicio
+
+# Identificar Variables
+Pi=3.14, radio, area
+
+# Recopilar información
+escribir("Ingresar radio de la circunferencia en cm: ")
+Leer(radio)
+
+# Calcular area del triangulo
+area=Pi*radio2
+
+# Mostrar los resultados del calculo
+Escribir("El area de la circunferencia es", area, "cm2")
+
+# Fin del programa
+fin
+
