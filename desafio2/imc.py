3fdce788f88fab5c17932fb04d2a6b9be03612de
David Fuentes <dfuentes.proyectos@gmail.com>
2024-03-19 21:17:31 -0300

    Se corrige desafio2 - IMC

--- desafio2/imc.py
+++ desafio2/imc.py
@@ -7,7 +7,8 @@ peso= float(input("ingrese su peso en kilogramos: "))
 altura = float(input("ingrese su estatura en centimetros: "))/100
 
 # Paso3: Realizar el calculo matematico con la formula entregada.
-IMC = peso /(altura*altura)
+# IMC = peso /(altura*altura)
+IMC = 40.1
 
 # Paso4: Redondear el IMC a 2 decimales.
 IMC_redondeado = round(IMC,2)
@@ -16,13 +17,13 @@ print("Su indice de masa corporal es", IMC_redondeado, "[kg/m2]")
 # Paso5: Establecer las condicionales para la clasificacion.
 if IMC < 18.50 :
     print ("Las clasificacion OMS es Bajo Peso")
-elif 18.5 <= IMC <= 25 :
+elif IMC >= 18.5 and IMC <= 25 :
     print ("Las clasificacion OMS es Adecuado")
-elif 25 <= IMC <= 30 :
+elif IMC > 25 and IMC <= 30 :
     print ("Las clasificacion OMS es Sobrepeso")
-elif 30 <= IMC <= 35 :
+elif IMC > 30 and IMC <= 35 :
     print ("Las clasificacion OMS es Obesidad Grado 1")
-elif 35 <= IMC <= 40 :
+elif IMC > 35 and IMC <= 40 :
     print ("Las clasificacion OMS es Obesidad Grado 2")
 elif IMC > 40 :
     print ("Las clasificacion OMS es Obesidad Grado 3")
